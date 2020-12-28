e91b74cbc07239d1e8728eefc48b20b63a5d35af
howardyang94 <howardyang94@gmail.com>
2020-12-27 16:35:21 -0800

    removed 's' from singlular units in time difference

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,5 +1,5 @@
 function timeDifference(current, previous) {
-    const milliSecondsPerMinute = 60 * 1000
+    const milliSecondsPerMinute = 60000
     const milliSecondsPerHour = milliSecondsPerMinute * 60
     const milliSecondsPerDay = milliSecondsPerHour * 24
     const milliSecondsPerMonth = milliSecondsPerDay * 30
@@ -10,25 +10,33 @@ function timeDifference(current, previous) {
     if (elapsed < milliSecondsPerMinute / 3) {
       return 'just now'
     }
-  
     if (elapsed < milliSecondsPerMinute) {
       return 'less than 1 min ago'
-    } else if (elapsed < milliSecondsPerHour) {
-      return Math.round(elapsed / milliSecondsPerMinute) + ' minutes ago'
+    } 
+    let str = '', num
+    if (elapsed < milliSecondsPerHour) {
+      num = Math.round(elapsed / milliSecondsPerMinute)
+      str += num + ' minute'
     } else if (elapsed < milliSecondsPerDay) {
-      return Math.round(elapsed / milliSecondsPerHour) + ' hours ago'
+      num = Math.round(elapsed / milliSecondsPerHour)
+      str += num + ' hour'
     } else if (elapsed < milliSecondsPerMonth) {
-      return Math.round(elapsed / milliSecondsPerDay) + ' days ago'
+      num = Math.round(elapsed / milliSecondsPerDay)
+      str += num + ' day'
     } else if (elapsed < milliSecondsPerYear) {
-      return Math.round(elapsed / milliSecondsPerMonth) + ' months ago'
+      num = Math.round(elapsed / milliSecondsPerMonth)
+      str += num + ' month'
     } else {
-      return Math.round(elapsed / milliSecondsPerYear) + ' years ago'
+      num = Math.round(elapsed / milliSecondsPerYear)
+      str += num + ' year'
     }
+    if(num > 1) {
+      // num will not be less than 1
+      str += 's'
+    }
+    return str + ' ago'
   }
   
   export function timeDifferenceForDate(date) {
-    //   const now = new Date().getTime()
-    // const updated = new Date(date).getTime()
     return timeDifference(new Date().getTime(), date)
-    // return newDate(date).getTime()
   }
